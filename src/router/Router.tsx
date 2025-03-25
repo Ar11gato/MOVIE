9f8f6b69201f5fb588d13a9494e95a0d1690ff02
Ar11gato <oper6662k19@mail.ru>
2025-03-25 20:30:00 +0300

    version 2

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from '../pages/HomePage/HomePage.tsx';
 import FavouritePages from '../pages/FavouritePages.tsx';
